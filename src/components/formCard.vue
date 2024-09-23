56858969723ad8f962334f48fceff37d190bb5fa
RichardNzembei <richardsonreuben78@gmail.com>
2024-09-23 22:19:45 +0300

    form data binding and emiting events commit

--- src/components/formCard.vue
+++ src/components/formCard.vue
@@ -1,16 +1,35 @@
 <script setup>
 import { ref } from "vue";
-const text = ref("");
-const checked = ref("");
+const FormData=ref({
+  name:'',
+  selected:[]
+})
+const emit=defineEmits(['submit'])
+function submitData(){
+  emit('submit',{...FormData.value})
+}
 </script>
 <template>
   <div class="form">
     <h4>form input binding</h4>
-    <input v-model.trim="text" />
-  <p>{{ text }}</p>
-  <h1>checkbox</h1>
-  <input type="checkbox" id="checkbox" v-model="checked" />
-  <label for="checkbox">{{ checked }}</label>
+    <p>citizens opinion</p>
+   <div class="formCard">
+    <form @submit.prevent="submitData">
+    <label class="label" for="visitorname" > Enter Name</label><br><br>
+    <input type="text" placeholder="Your Name.." v-model="FormData.name"><br><br>
+    <label class="label" for="Politicalstand">Political Stand</label><br><br>
+    <select name="politicalstand" id="politics" v-model="FormData.selected" multiple >
+      <option disabled value=""><u>leave your opinion!</u></option>
+      <option value="I support the government">I support the government</option>
+      <option value="i don't support the government">i don't support the government</option>
+      <option value="Tired">Tired</option>
+      <option value="Not really interested">Not really interested</option>
+      <option value="other">other</option>
+    </select>
+<button type="submit">submit data</button>
+  </form>
+   </div>
+
   </div>
  
 </template>
@@ -21,9 +40,30 @@ const checked = ref("");
   margin: 5px 5px ;
   width: 300px;
 }
+.formCard{
+  padding: 10px;
+  
+}
+.label{
+  color: rgb(124, 136, 136);
+  font-weight: bolder;
+}
+input{
+  border-radius: 5px;
+  text-decoration: none;
+  border: none;
+  padding: 10px;
+}
 h4{
     font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
     text-decoration: underline;
     font-size: 20px;
+}
+button{
+  margin: 10px;
+  border: none;
+  background-color: rgba(16, 17, 17, 0.849);
+  color: white;
+  border-radius: 4px;
 }
 </style>
--- src/views/basicsDashboard.vue
+++ src/views/basicsDashboard.vue
@@ -1,4 +1,5 @@
 <script setup>
+import {ref} from 'vue'
 import listCard from "@/components/listCard.vue";
 import formCard from "@/components/formCard.vue";
 import conditionCard from "@/components/conditionCard.vue";
@@ -11,6 +12,18 @@ const toHomePage = () => {
 const toComponentsPage = () => {
   router.push("/componentsdashboard");
 };
+const submitedData=ref({
+  name:'',
+  selected:[]
+})
+const submitData=(data)=>{
+  submitedData.value=data
+}
+const showReview=ref(false)
+const toggleReview=()=>{
+  showReview.value=!showReview.value
+}
+
 </script>
 <template>
   <div class="basicsProjects">
@@ -23,7 +36,9 @@ const toComponentsPage = () => {
         <listCard />
       </div>
       <div class="sections">
-        <formCard />
+        <formCard 
+        @submit="submitData"
+        />
       </div>
       <div class="sections">
         <conditionCard />
@@ -32,12 +47,44 @@ const toComponentsPage = () => {
         <eventCard />
       </div>
     </div>
+    <div class="outputs">
+      <span class="outputHeading">outputs</span>
+      <div class="formoutput">
+        <h1>form output</h1>
+        <p>click review to see citizens opinion!</p>
+        <div class="citizenreview">
+        
+          <strong><span class="review" @click="toggleReview">review</span></strong><br><br>
+          <div v-if="showReview">
+            <span>{{ submitedData.name }}</span>
+            <p>{{ submitedData.selected.join() }}</p>
+          </div>
+        
+        </div>
+       
+      </div>
+
+    </div>
   </div>
 </template>
 <style scoped>
+.review{
+  margin: 5px;
+ 
+  color: rgb(187, 107, 107);
+  text-decoration: underline;
+  font-weight:bolder;
+  width: 100px;
+}
+.citizenreview{
+  background-color: azure;
+  width: max-content;
+  padding: 10px 0;
+}
 .basicsProjects {
   background-color: rgb(252, 249, 249);
   padding: 10px 10px;
+  height: 100%;
 }
 .basicsbody {
   padding: 20px;
@@ -55,5 +102,20 @@ button {
   margin: 10px;
   background-color: rgb(221, 221, 209);
   border-radius: 3px;
+}
+.formoutput{
+  background-color: white;
+  width: 300px;
+  height: 500px;
+}
+.outputHeading{
+  font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+  font-size: 20px;
+}
+h1{
+  font-size: 25px;
+  padding: 10px;
+  margin: 10px 10px;
+  color: rgb(84, 111, 134);
 }
 </style>
